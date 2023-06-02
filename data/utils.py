c0a50748e9185e71009631f5f63d4699f5536564
Leolty <til040@ucsd.edu>
2023-06-02 00:42:45 +0000

    add more data utils

--- data/utils.py
+++ data/utils.py
@@ -62,8 +62,114 @@ def load_data(task:str, language:str, settings: Union[str, list]):
         with gzip.open(f"data/{task}/{language}/{setting}.gz", 'rb') as f:
             data[setting] = pickle.load(f)
     
-    return [data[setting] for setting in settings]
+    if len(settings) == 1:
+        return data[settings[0]]
+    else:
+        return [data[setting] for setting in settings]
     
+def construct_trainable_data(dic: dict, language: str = "python"):
+    """
+    construct the data that can be used for training
+
+    Args:
+        dic: the dictionary of data from load_data
+        language: the language of the data
     
+    Returns:
+        return_data: the data that can be used for training (a list of dictionaries)
+    """
+    # data is a dictionary with keys: file_path, context, import_statement, code, next_line
+    
+    # check if the language is valid
+    assert language in ["python", "java"], "language must be one of [python, java]"
+
+    return_data = {}
+
+    for key, data_list in dic.items():
+
+        return_data[key] = []
+
+        for data in data_list:
+            # check if context is a string
+            assert isinstance(data['context'], str), "context must be a string instead of snippet list"
+
+            if language == "python":
+                path = f"# Path: {data['file_path']}"
+            
+            elif language == "java":
+                path = f"// Path: {data['file_path']}"
+
+            d = f"""{data['context']}
+{path}
+{data['import_statement']}
+
+{data['code']}"""
+
+            label = data['next_line']
+
+            return_data[key].append({ 'data': d, 'label': label })
+        
+    return return_data
+
+
+# crop lines given a threshold and tokenized code
+def crop_code_lines(code: Union[str, list], threshold: int):
+    """
+    Crop the code to the last threshold lines.
+
+    Args:
+        code: the code to crop (either a string or a list of tokens)
+        threshold: the number of lines to keep
+    
+    Returns:
+        code: the cropped code
+    """
+
+    # if the code is string, meaning it is not tokenized
+    if isinstance(code, str):
+        code = code.split('\n')
+
+        # return the last threshold lines if the code is longer than the threshold
+        if len(code) > threshold:
+            return "\n".join(code[-threshold:])
+        else:
+            return "\n".join(code)
+    
+    # if the code is tokenized
+    elif isinstance(code, list):
+        # set the current number of lines to -1, since the last line is always a newline
+        cur_lines = -1
+        # iterate over the code list from the end to the beginning
+        for i in range(len(code)-1, -1, -1):
+            # "Ċ" is the token for newline 
+            if "Ċ" in code[i]:
+                cur_lines += 1
+            
+            # if the current number of lines reaches the threshold, 
+            # return the code from the current index to the end (do not include the newline token)
+            if cur_lines == threshold:
+                return code[i+1:]
+        
+        # if the code is shorter than the threshold, return the whole code
+        return code
+
+# comment code
+def comment(code: str, language: str):
+    """
+    Comment the code.
+
+    Args:
+        code: the code to comment
+        language: the language of the code
+    
+    Returns:
+        code: the commented code
+    """
+    if language == "python":
+        return "\n".join([f"# {line}" for line in code.split("\n")])
+    elif language == "java":
+        return "\n".join([f"// {line}" for line in code.split("\n")])
+    else:
+        raise ValueError("language must be one of [python, java]")
         
 
