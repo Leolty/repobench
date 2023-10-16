4e4c4c837f81b24da5929c6b09908d35ea59d3dc
Leolty <til040@ucsd.edu>
2023-10-16 09:33:59 +0000

    🏄‍♂️ change the data name

--- archive_data/utils.py
+++ archive_data/utils.py
@@ -1,24 +1,33 @@
 import gzip
 import pickle
+import json
 from tqdm import tqdm
-from typing import Union
+from typing import Union, Optional
 import os
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-def load_data(task:str, language:str, settings: Union[str, list]):
+def load_data(split:str, task:str, language:str, settings: Optional[Union[str, list]], length: Optional[str] = None):
     """
     Load data from the specified task and language.
 
     Args:
-        task: the task to load
-        language: the language to load
-        settings: the settings to load
+        split: the split to load (train or test)
+        task: the task to load (retrieval/r, completion/c, pipeline/p)
+        language: the language to load (python or java)
+        settings: the settings to load (xf-f, xf-r, or if)
     
     Returns:
         data: the loaded data
     """
 
+    # some assertions
+
+    # split must be one of train or test
+    assert split.lower() in ['train', 'test'], \
+        "split must be one of train or test"
+
+    # task must be one of r/R/retrieval, c/C/completion, or p/P/pipeline
     if task.lower() == 'r':
         task = 'retrieval'
     elif task.lower() == 'c':
@@ -26,9 +35,18 @@ def load_data(task:str, language:str, settings: Union[str, list]):
     elif task.lower() == 'p':
         task = 'pipeline'
     
+    # language must be one of python or java
     if language.lower() == 'py':
         language = 'python'
     
+    # settings must be one of xf-f, xf-r, or if or a list of their combinations
+    if not settings:
+        # if settings is not specified, use all settings
+        if task == "retrieval":
+            settings = ['cross_file_first', 'cross_file_random']
+        else:
+            settings = ['cross_file_first', 'cross_file_random', 'in_file']
+        
     if isinstance(settings, str):
         settings = [settings]
     
@@ -41,83 +59,67 @@ def load_data(task:str, language:str, settings: Union[str, list]):
             settings[i] = 'in_file'
         
 
-    # some assertions
+    # check if the task is valid
     assert task.lower() in ['r', 'c', 'p', 'retrieval', 'completion', 'pipeline'], \
         "task must be one of R, C, or P"
     
-
+    # check if the language is valid
     assert language.lower() in ['python', 'java', 'py'], \
         "language must be one of python, java"
 
-    
+
     if task == "retrieval":
         assert all([setting.lower() in ['cross_file_first', 'cross_file_random'] for setting in settings]), \
             "For RepoBench-R, settings must be one of xf-f or xf-r"
     else:
         assert all([setting.lower() in ['cross_file_first', 'cross_file_random', 'in_file'] for setting in settings]), \
             "Settings must be one of xf-f, xf-r, or if"
+        
+        if length:
+            assert length.lower() in ['2k', '8k'], \
+                "length must be one of 2k or 8k"
+    
+        # for completion, length must be specified
+        if task == "completion":
+            assert length is not None, "length must be specified for completion task"
     
 
     # load data
-    data = {}
-    # We split retrieval data into shards due to the github file size limit
-    if task == "retrieval":
+    if split == "train":
+        data = {}
+        
         for setting in tqdm(settings, desc=f"Loading data"):
-            # we only further split the cross_file_first setting for java
-            if setting == "cross_file_first" and language == "java":
-                dic = {
-                    "train": {},
-                    "test": {}
-                }
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_easy_1.gz", 'rb') as f:
-                    train_easy_1 = pickle.load(f)
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_easy_2.gz", 'rb') as f:
-                    train_easy_2 = pickle.load(f)
-                dic['train']['easy'] = train_easy_1['easy'] + train_easy_2['easy']
-
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_hard_1.gz", 'rb') as f:
-                    train_hard_1 = pickle.load(f)
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_hard_2.gz", 'rb') as f:
-                    train_hard_2 = pickle.load(f)
-                dic['train']['hard'] = train_hard_1['hard'] + train_hard_2['hard']
-
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_test.gz", 'rb') as f:
-                    test = pickle.load(f)
-                dic['test'] = test['test']
-        
-            
-                data[setting] = dic
-        
-            else:
-                dic = {
-                    "train": {},
-                    "test": {}
-                }
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_easy.gz", 'rb') as f:
-                    train_easy = pickle.load(f)
-                dic['train']['easy'] = train_easy['easy']
-
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_train_hard.gz", 'rb') as f:
-                    train_hard = pickle.load(f)
-                dic['train']['hard'] = train_hard['hard']
-
-                with gzip.open(f"{ROOT}/{task}/{language}/{setting}_test.gz", 'rb') as f:
-                    test = pickle.load(f)
-                dic['test'] = test['test']
-        
-                data[setting] = dic
-
-    else:
-        for setting in tqdm(settings, desc=f"Loading data"):
-            with gzip.open(f"{ROOT}/{task}/{language}/{setting}.gz", 'rb') as f:
+            with gzip.open(f"{ROOT}/train/{task}/{language}/{setting}.pkl.gz", 'rb') as f:
                 data[setting] = pickle.load(f)
-    
+
+                # it has train, dev, and test, we combine them together
+                if task == "retrieval":
+                    # retrieval has easy and hard for each split
+                    data[setting] = {
+                        "easy": data[setting]['train']['easy'] + data[setting]['dev']['easy'] + data[setting]['test']['easy'],
+                        "hard": data[setting]['train']['hard'] + data[setting]['dev']['hard'] + data[setting]['test']['hard']
+                    }
+                elif task == "completion":
+                    data[setting] = data[setting]['train'] + data[setting]['dev'] + data[setting]['test']
+        
+
+    elif split == "test":
+        data = {}
+        
+        for setting in tqdm(settings, desc=f"Loading data"):
+            if length not in ['2k', '8k'] and task != "completion":
+                with gzip.open(f"{ROOT}/test/{task}/{language}/{setting}.gz", 'rb') as f:
+                    data[setting] = pickle.load(f)
+            else:
+                with gzip.open(f"{ROOT}/test/{task}/{language}/{length}/{setting}.gz", 'rb') as f:
+                    data[setting] = pickle.load(f)
+        
     if len(settings) == 1:
         return data[settings[0]]
     else:
         return [data[setting] for setting in settings]
     
-def construct_trainable_data(dic: dict, language: str = "python"):
+def construct_trainable_data(data: list, language: Optional[str] = None):
     """
     construct the data that can be used for training
 
@@ -131,33 +133,31 @@ def construct_trainable_data(dic: dict, language: str = "python"):
     # data is a dictionary with keys: file_path, context, import_statement, code, next_line
     
     # check if the language is valid
+    assert language is not None, "language must be set"
+
     assert language in ["python", "java"], "language must be one of [python, java]"
 
-    return_data = {}
+    return_data = []
 
-    for key, data_list in dic.items():
+    for d in data:
+        # check if context is a string
+        assert isinstance(d['context'], str), "context must be a string instead of snippet list"
 
-        return_data[key] = []
+        if language == "python":
+            path = f"# Path: {d['file_path']}"
+        
+        elif language == "java":
+            path = f"// Path: {d['file_path']}"
 
-        for data in data_list:
-            # check if context is a string
-            assert isinstance(data['context'], str), "context must be a string instead of snippet list"
-
-            if language == "python":
-                path = f"# Path: {data['file_path']}"
-            
-            elif language == "java":
-                path = f"// Path: {data['file_path']}"
-
-            d = f"""{data['context']}
+        _d = f"""{d['context']}
 {path}
-{data['import_statement']}
+{d['import_statement']}
 
-{data['code']}"""
+{d['code']}"""
 
-            label = data['next_line']
+        label = d['next_line']
 
-            return_data[key].append({ 'data': d, 'label': label })
+        return_data.append({ 'data': _d, 'label': label })
         
     return return_data
 
@@ -221,5 +221,3 @@ def comment(code: str, language: str):
         return "\n".join([f"// {line}" for line in code.split("\n")])
     else:
         raise ValueError("language must be one of [python, java]")
-        
-
